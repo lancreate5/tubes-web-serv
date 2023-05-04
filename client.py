fd0e53c63143f831ca17cef31e967e9309a3f111
Allen <valternatean13@gmail.com>
2023-05-04 13:40:35 +0700

    feat: request data via client.py
    Now user could request html file by running client.py

--- client.py
+++ client.py
@@ -1,30 +1,44 @@
 import socket
 from sys import exit
 
+def generate_HTTP_GET_request(filename):
+    header = f"GET /{filename} HTTP/1.1\n"
+    header += "Host: client.py\n"
+    header += "Agent: terminal\n"
+
+    return header
+
 def main():
     server_name = "localhost"
     server_port = 8000
-    client_socket = socket.socket(
-        socket.AF_INET, socket.SOCK_STREAM
-    )
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-    # Connect client to server
+    # Connect client to server. Handle the case where the 
+    # server is offline
     try:
         client_socket.connect((server_name, server_port))
     except ConnectionRefusedError:
         print("Check your server connection!")
         exit(0)
 
-    temp = input("Masukkan teks:")
-    while temp != "end":
-        # Send this to server
-        client_socket.send(temp.encode())
+    while True:
+        # Ask user what file to be sent
+        filename = input("Masukkan nama file (contoh: index.html): ")
 
-        # Enter next item
-        temp = input("Masukkan teks:")
+        # Buat dan kirimkan pesan HTTP GET ke server
+        client_socket.send(generate_HTTP_GET_request(filename).encode())
+          
+        # Terima pesan (paket) yang dikirimkan oleh server
+        buffer = ""
+        while True:
+            message = client_socket.recv(1024)
+            if not(message): break
+            buffer += message.decode()
+        print(buffer)
 
-    # Close connection
-    client_socket.close()
+        # Close connection
+        client_socket.close()
+        exit(0)
     
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,26 +1,44 @@
 import socket
+from sys import exit
+
+def send_404():
+    with open("/index.html") as file:
+        return file.read()
+
+def handle_GET():
+    content = ""
+    try:
+        with open("./index.html") as file:
+            content = file.read()
+    except FileNotFoundError:
+        send_404()
+    return content
 
 def main():
-    server_socket = socket.socket(
-        socket.AF_INET, socket.SOCK_STREAM
-    )
-    
-    # Make the socket to listen to this port
-    server_socket.bind(('', 8000))
-    
-    # Make the socket accept any connection
+    server_port = 8000
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('', server_port))
     server_socket.listen(1)
+    print("Server's ready!")
     
-    message = ""
-    while message.lower() != "end":
+    while True:
         # Accept any connection
-        connected_socket, address = server_socket.accept()
-
-        message = connected_socket.recv(8).decode()
-        message += connected_socket.recv(8).decode()
-        print(f"received from {address}: {message}")
+        connected_socket, _ = server_socket.accept()
+        
+        message = connected_socket.recv(1024).decode()
+        if message == "GET":
+            content = handle_GET()
+            print(len(content))
+            for i in range(0, len(content)):
+                connected_socket.send(content[i].encode())
+            print("hell")
+            connected_socket.send("\r\n".encode())
+        else:
+            print("apaan dah")
+            exit(0)
 
         connected_socket.close()
+        server_socket.listen(0)
     
 if __name__ == '__main__':
     main()
