3eb26d7b7adb5a483a9982287696704cd6e98fc3
Allen <valternatean13@gmail.com>
2023-05-22 10:58:52 +0700

    feat: change color on button hover

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let query = (tag) => document.querySelector(tag);
 let selectCls = (cls) => document.getElementsByClassName(cls)[0];
 let onClick = (obj, fn) => obj.addEventListener("click", fn);
 let onHover = (obj, fn) => obj.addEventListener("mouseover", fn);
+let onOut = (obj, fn) => obj.addEventListener("mouseout", fn);
 
 color = [
 	// wholeBG |   text   |   box    | Highlighting
@@ -12,20 +13,27 @@ color = [
 
 let token = 0;
 let button = selectCls("swap-color");
-onClick(button, () => {
-	token = (token + 1) % 2;
+let eventID;
 
-	query("body").style.background = color[token][0];
+onHover(button, () => {
+		eventID = setInterval(() => {
+			token = (token + 1) % 2;
 
-	let box = selectCls("wrap");
-	box.style.background = color[token][2];
+			query("body").style.background = color[token][0];
 
-	button.style.color = color[token][1];
-	button.style.background = color[token][0];
+			let box = selectCls("wrap");
+			box.style.background = color[token][2];
 
-	let content = selectCls("content").children;
-	for(let i = 0; i < content.length; i++) {
-		content[i].style.color = color[token][1];
+			button.style.color = color[token][1];
+			button.style.background = color[token][0];
+
+			let content = selectCls("content").children;
+			for(let i = 0; i < content.length; i++) {
+				content[i].style.color = color[token][1];
+			}
+		},
+		100);
 	}
-});
+);
 
+onOut(button, () => clearInterval(eventID));
